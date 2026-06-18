6473bfca3b5b765b1020311226e52933cd2bb890
Darragh Boylan <darraghdboylan@gmail.com>
2026-06-18 10:43:17 +0200

    1st tests

---
+++ src/test/java/com/example/demo/web/tetstStatistiqueController.java
@@ -0,0 +1,16 @@
+package com.example.demo.web;
+
+import com.example.demo.data.Voiture;
+import com.example.demo.service.Echantillon;
+import com.example.demo.service.Statistique;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
