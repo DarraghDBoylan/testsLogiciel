76336d054e482698b5b90021f51acc185856f419
Darragh Boylan <darraghdboylan@gmail.com>
2026-06-18 10:52:36 +0200

    1st test du tp3 mais ca marche cette fois

--- src/test/java/com/example/demo/web/tetstStatistiqueController.java
+++ src/test/java/com/example/demo/web/tetstStatistiqueController.java
@@ -14,3 +14,33 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@WebMvcTest(StatistiqueController.class) 
+class StatistiqueControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc; 
+
+    @MockBean
+    private Statistique statistique; 
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
+    @Test
+    void testGetStatistiques_Succes() throws Exception {
+        // 1. ARRANGE : On prépare un faux échantillon que le service va retourner
+        Echantillon fauxEchantillon = new Echantillon(5, 20000);
+        when(statistique.prixMoyen()).thenReturn(fauxEchantillon);
+
+        // 2. ACT & ASSERT : On simule le GET /statistique
+        mockMvc.perform(get("/statistique"))
+                .andExpect(status().isOk()) // Vérifie le code HTTP 200
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON)) // Vérifie que c'est du JSON
+                .andExpect(jsonPath("$.nombreDeVoitures").value(5)) // Vérifie le contenu JSON
+                .andExpect(jsonPath("$.prixMoyen").value(20000));
+
+        verify(statistique, times(1)).prixMoyen();
+    }
